da11925a98ba7abd81c69f86b69f52d52bf8dd38
imohammad-al <ee.contactor@gmail.com>
2026-07-06 13:16:27 +0330

    Ignore build files

--- build/Desktop_x86_windows_msys_pe_64bit_Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_ProteusClone_FILE A:/Projects/QTProjects/ProteusClone/ProteusClone/build/Desktop_x86_windows_msys_pe_64bit_Debug/ProteusClone.exe)
-set(__QT_DEPLOY_TARGET_ProteusClone_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_ProteusClone_RUNTIME_DLLS A:/Qt/6.8.3/mingw_64/bin/Qt6Widgets.dll;A:/Qt/6.8.3/mingw_64/bin/Qt6Gui.dll;A:/Qt/6.8.3/mingw_64/bin/Qt6Core.dll)
--- build/Desktop_x86_windows_msys_pe_64bit_Debug/.qt/deploy_ProteusClone_43183f7184.cmake
+++
@@ -1,8 +0,0 @@
-include("A:/Projects/QTProjects/ProteusClone/ProteusClone/build/Desktop_x86_windows_msys_pe_64bit_Debug/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/ProteusClone-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE A:/Projects/QTProjects/ProteusClone/ProteusClone/build/Desktop_x86_windows_msys_pe_64bit_Debug/ProteusClone.exe
-    GENERATE_QT_CONF
-)
